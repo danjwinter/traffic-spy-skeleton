26fb6487be2c373e99d706899753376f642bf93a
Dan Winter <dan.j.winter@gmail.com>
2015-12-12 13:35:58 -0700

    added http auth, stil in progress

--- app/controllers/server.rb
+++ app/controllers/server.rb
@@ -1,7 +1,37 @@
+require 'uri'
+
 module TrafficSpy
+
   class Server < Sinatra::Base
+
+    use Rack::Auth::Basic, "Protected Area" do |username, password|
+      if User.exists?(identifier: username) && User.find_by(identifier: username).password == nil
+        User.find_by(identifier: username).update(password: password)
+      elsif User.exists?(identifier: username)
+        password == User.find_by(identifier: username).password
+      else
+        "Username does not exist. Must register user."
+        false
+      end
+    end
+    # 
+    # helpers do
+    #   def protected!
+    #     return if authorized?
+    #     headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+    #     halt 401, "Not authorized\n"
+    #   end
+    #
+    #   def authorized?
+    #     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+    #     # binding.pry
+    #     @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [User.find_by(identifier: @auth.credentials[0]), User.find_by(identifier: @auth.credentials[0]).password]
+    #   end
+    # end
+
     get '/' do
-      erb :index
+      # protected!
+  erb :index
     end
 
     not_found do
@@ -9,6 +39,7 @@ module TrafficSpy
     end
 
     get '/sources/:id' do |id|
+
       if @user = TrafficSpy::User.find_by(identifier: id)
         if @user.payloads.count == 0
           erb :no_payload_data, locals: {id: id}
